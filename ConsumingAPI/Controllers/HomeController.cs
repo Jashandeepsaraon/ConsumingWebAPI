63a8cde227fef69ec7c609dc3c42150e251d3e83
Jashan Saraon <Jashanp.k99@gmail.com>
2019-06-02 11:20:52 -0500

    Upload the Project.

--- ConsumingAPI/Controllers/HomeController.cs
+++ ConsumingAPI/Controllers/HomeController.cs
@@ -53,7 +53,8 @@ namespace ConsumingAPI.Controllers
             var response = httpClient.GetAsync(url).Result;
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                return RedirectToAction(nameof(AccountController.LogOut));
+                return View();
+                //return RedirectToAction(nameof(HomeController.GetAll));
             }
             else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
@@ -70,14 +71,14 @@ namespace ConsumingAPI.Controllers
 
         [HttpGet]
         public ActionResult CreateHouseHold()
-        {           
+        {
             return View();
         }
 
         [HttpPost]
         public ActionResult CreateHouseHold(HouseholdsViewModel formdata)
         {
-      
+
             var cookie = Request.Cookies["MyFirstCookie"];
 
             if (cookie == null)
@@ -129,5 +130,228 @@ namespace ConsumingAPI.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public ActionResult EditHouseHold()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditHouseHold(int id, HouseholdsViewModel formdata)
+        {
+            var cookie = Request.Cookies["MyFirstCookie"];
+
+            if (cookie == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var token = cookie.Value;
+
+            var url = "http://localhost:49995/api/Households/{id}";
+
+            // HttpClient object to handle the comunication
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization",
+                $"Bearer {token}");
+
+            var name = formdata.Name;
+            var description = formdata.Description;
+            var dateUpdated = DateTime.Now;
+
+            //Parameters list with KeyValue pair
+            var parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("Name", name));
+            parameters.Add(new KeyValuePair<string, string>("Description", description));
+
+            //Encoding the parameters before sending to the API
+            var encodedParameters = new FormUrlEncodedContent(parameters);
+
+            //Calling the API and storing the response
+            var response = httpClient.PostAsync(url, encodedParameters).Result;
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                //Read the response
+                var data = response.Content.ReadAsStringAsync().Result;
+
+                //Convert the data back into an object
+                var result = JsonConvert.DeserializeObject<Households>(data);
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var data1 = response.Content.ReadAsStringAsync().Result;
+                var result1 = JsonConvert.DeserializeObject<APIErroData>(data1);
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return View("Error");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DisplayUsers(int? id)
+        {
+            var cookie = Request.Cookies["MyFirstCookie"];
+
+            if (cookie == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var token = cookie.Value;
+
+            var url = "http://localhost:49995/api/Households/DisplayUsers/{id}";
+
+            // HttpClient object to handle the comunication
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization",
+                $"Bearer {token}");
+            var response = httpClient.GetAsync(url).Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return View();
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var data1 = response.Content.ReadAsStringAsync().Result;
+                var result1 = JsonConvert.DeserializeObject<APIErroData>(data1);
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return View("Error");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult InviteUsers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InviteUsers(int? id)
+        {
+            var cookie = Request.Cookies["MyFirstCookie"];
+
+            if (cookie == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var token = cookie.Value;
+
+            var url = "http://localhost:49995/api/Households/InviteUsers/{id}?email={email}";
+
+            // HttpClient object to handle the comunication
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization",
+                $"Bearer {token}");
+            var response = httpClient.GetAsync(url).Result;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return View();
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var data1 = response.Content.ReadAsStringAsync().Result;
+                var result1 = JsonConvert.DeserializeObject<APIErroData>(data1);
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return View("Error");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult JoinHouseHold(int? id)
+        {
+            var cookie = Request.Cookies["MyFirstCookie"];
+
+            if (cookie == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var token = cookie.Value;
+
+            var url = "http://localhost:49995/api/Households/JoinHousehold/{id}";
+
+            // HttpClient object to handle the comunication
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization",
+                $"Bearer {token}");
+            var response = httpClient.GetAsync(url).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                ModelState.AddModelError("", "You are not Invited to this HouseHold.");
+                return View();
+            }
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return View();
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var data1 = response.Content.ReadAsStringAsync().Result;
+                var result1 = JsonConvert.DeserializeObject<APIErroData>(data1);
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return View("Error");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            var cookie = Request.Cookies["MyFirstCookie"];
+
+            if (cookie == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var token = cookie.Value;
+
+            var url = "http://localhost:49995/api/Households/{id}";
+
+            // HttpClient object to handle the comunication
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Authorization",
+                $"Bearer {token}");
+            var response = httpClient.GetAsync(url).Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                ModelState.AddModelError("", "You are not Invited to this HouseHold.");
+                return View();
+            }
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return View();
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var data1 = response.Content.ReadAsStringAsync().Result;
+                var result1 = JsonConvert.DeserializeObject<APIErroData>(data1);
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return View("Error");
+            }
+            return View();
+        }
+
     }
 }
